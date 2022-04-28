399fe5d82de8360049b518c13595080c2177f5bf
Ian-M-Moffett <ian@kesscoin.com>
2022-04-28 19:03:02 -0400

    Hello syscall!

--- userspace/usentry.asm
+++ userspace/usentry.asm
@@ -4,10 +4,14 @@ global userspace_entry
 
 userspace_entry:
     mov rax, 0x2
-    mov r14, 50
-    mov r13, 50
-    mov r12, 150
-    mov r11, 150
+    mov r14, 0
+    mov r13, 0
+    mov r12, 500
+    mov r11, 30
     mov r10, 0xFFFFFF
     int 0x80
+    
+    mov rax, 0x1
+    int 0x80
+
     jmp $
