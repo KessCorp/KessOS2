93ba97246220a1456f582461abdf4e51dc6aabaf
Ian-M-Moffett <ian@kesscoin.com>
2022-04-28 17:52:36 -0400

    Square!

--- userspace/usentry.asm
+++ userspace/usentry.asm
@@ -3,6 +3,11 @@ global userspace_entry
 
 
 userspace_entry:
-    mov rax, 0x1
+    mov rax, 0x2
+    mov r14, 50
+    mov r13, 50
+    mov r12, 150
+    mov r11, 150
+    mov r10, 0xFFFFFF
     int 0x80
     jmp $
