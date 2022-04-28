b96e1647d6b58c7b319172d5fbe56c1520f75f26
Ian-M-Moffett <ian@kesscoin.com>
2022-04-28 19:43:55 -0400

    Draw a little bar.

--- userspace/ui.c
+++ userspace/ui.c
@@ -1,4 +1,15 @@
 void ui_start() {
     __asm__ __volatile__("mov $0x1, %rax; int $0x80");
+
+    // Draw a little bar.
+    __asm__ __volatile__(
+                        "mov $0x2, %rax\n"
+                        "mov $0, %r14\n"
+                        "mov $0, %r13\n"
+                        "mov $500, %r12\n"
+                        "mov $30, %r11\n"
+                        "mov $0xFFFFFF, %r10\n"
+                        "int $0x80"
+                        );
     while (1);
 }
