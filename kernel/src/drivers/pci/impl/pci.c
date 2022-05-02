72086d76779eb44146bfb205e6f211412ffeb925
Ian-M-Moffett <ian@kesscoin.com>
2022-05-02 02:43:12 -0400

    Added new tools!

--- kernel/src/drivers/pci/impl/pci.c
+++ kernel/src/drivers/pci/impl/pci.c
@@ -36,7 +36,7 @@ SECTION_DATA static const char* const BASE_CLASS_TYPES[] = {
 };
 
 
-static uint8_t vendor_is_valid(uint16_t vendor_id) {
+uint8_t vendor_is_valid(uint16_t vendor_id) {
     return vendor_id != 0xFFFF;
 }
 
@@ -92,6 +92,10 @@ void pci_enumerate_and_log() {
 }
 
 
+uint8_t pci_vendor_is_valid(uint16_t vendor_id) {
+    return vendor_id != 0xFFFF;
+}
+
 
 uint16_t pci_get_vendor_id(uint8_t bus, uint8_t slot, uint8_t func) {
     return pci_read_word(bus, slot, func, 0x0);
@@ -120,4 +124,30 @@ uint8_t pci_get_prog_if(uint8_t bus, uint8_t slot, uint8_t func) {
 
 uint8_t pci_get_revision_id(uint8_t bus, uint8_t slot, uint8_t func) {
     return pci_read_word(bus, slot, func, 0x8) & 0x00FF;
+}
+
+
+struct PCIDevice pci_loop_through(uint8_t i) {
+    for (int bus = 0 + i; bus < 255; ++bus) {
+        for (int slot = 0; slot < 32; ++slot) {
+            for (int func = 0; func < 8; ++func) {
+                if (pci_vendor_is_valid(pci_read_word(bus, slot, func, 0))) {
+                    struct PCIDevice dev = {
+                        .bus = bus,
+                        .slot = slot,
+                        .func = func,
+                        .valid = 1
+                    };
+
+                    return dev;
+                }
+            }
+        }
+    }
+
+    struct PCIDevice invalid_dev = {
+        .valid = 0
+    };
+    
+    return invalid_dev;         // No device found!
 }
--- kernel/src/drivers/pci/pci.h
+++ kernel/src/drivers/pci/pci.h
@@ -7,6 +7,15 @@
 #include <arch/io/io.h>
 #include <stdint.h>
 
+
+struct PCIDevice {
+    uint8_t bus;
+    uint8_t slot;
+    uint8_t func;
+    uint8_t valid;
+};
+
+
 uint16_t pci_read_word(uint8_t bus, uint8_t slot, uint8_t func, uint8_t offset);
 uint16_t pci_get_vendor_id(uint8_t bus, uint8_t slot, uint8_t func);
 uint16_t pci_get_device_id(uint8_t bus, uint8_t slot, uint8_t func);
@@ -14,6 +23,8 @@ uint8_t pci_get_classid(uint8_t bus, uint8_t slot, uint8_t func);
 uint8_t pci_get_subclass_id(uint8_t bus, uint8_t slot, uint8_t func);
 uint8_t pci_get_prog_if(uint8_t bus, uint8_t slot, uint8_t func);
 uint8_t pci_get_revision_id(uint8_t bus, uint8_t slot, uint8_t func);
+uint8_t pci_vendor_is_valid(uint16_t vendor_id);
+struct PCIDevice pci_loop_through(uint8_t i);
 void pci_enumerate_and_log();
 
 
