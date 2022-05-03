21beaa023d14301a54a063cfa46b10c4cb3869c5
Ian-M-Moffett <ian@kesscoin.com>
2022-05-03 13:56:19 -0400

    Added some structures for later.

--- kernel/src/drivers/usb/host/impl/xhci.c
+++ kernel/src/drivers/usb/host/impl/xhci.c
@@ -7,6 +7,36 @@
 static struct PCIDevice xhci_controller;
 static uint64_t mmio_addr;
 
+static struct Controller {
+    struct CapRegs {
+        uint8_t caplength;
+        uint8_t rsvd;
+        uint16_t hciversion;
+        uint32_t hcsparams1;
+        uint32_t hcsparams2;
+        uint32_t hccparams1;
+        uint32_t dboff;
+        uint32_t rtsoff;
+        uint32_t hccparams2;
+    } cap_regs;
+    
+    struct OpRegs {
+        uint32_t usbcmd;
+        uint32_t usbsts;
+        uint64_t dnctrl;
+        uint32_t cncr;
+        uint64_t dcbaap;
+        uint64_t config;
+    } op_regs;
+
+    struct PortRegs {
+        uint32_t portsc;
+        uint32_t portpmsc;
+        uint32_t portli;
+        uint32_t reserved;
+    } port_regs;
+} controller;
+
 
 uint8_t xhci_init() {
     for (int bus = 0; bus < PCI_HIGHEST_BUS; ++bus) {
