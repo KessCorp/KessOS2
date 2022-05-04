547aa6329a369724cb5b2d8903a4225ac9c03b1d
Ian-M-Moffett <ian@kesscoin.com>
2022-05-04 12:48:41 -0400

    Added a macro for some indirection between userspace functions and kernelspace functions.

--- kernel/src/util/asm.h
+++ kernel/src/util/asm.h
@@ -6,6 +6,7 @@
 #define HLT __asm__ __volatile__("hlt")
 #define CLI __asm__ __volatile__("cli")
 #define STI __asm__ __volatile__("sti")
+#define USER_INDIRECTION __asm__ __volatile__("cli; sti")     // Used so functions cannot be called directly from userspace.
 #define SECTION_DATA __attribute__((section(".data")))
 
 #endif
